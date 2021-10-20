2cedba1e27726632bc365e0f34073b42bcc593c5
gretamiller <gretaamymiller@gmail.com>
2021-10-20 14:38:58 +0100

    issue #7 add model class

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,17 +1,19 @@
-#
-# Model class
-#
-
+'''Create the model class'''
 class Model:
-    """A Pharmokinetic (PK) model
+    """Creates the model class.
 
-    Parameters
-    ----------
-
-    value: numeric, optional
-        an example paramter
+    :num_components: number of peripherial compartments
+    :dose_type: either 'c' (continuous), 'i' (instantaneous), or 'both'
+    :self.compartments: list of compartment objects in model
+    :self.doses : list of dose objects in model
 
     """
-    def __init__(self, value=42):
-        self.value = value
+    def __init__(self, num_compartments = 1, dose_type = 'i', compartments = [], doses = []):
+        self.num_compartments = num_compartments
+        self.dose_type = dose_type
+        self.compartments = compartments
+        self.doses = doses
+
+    def __str__(self):
+        return 'This model has {self.num_compartments} peripherial compartments with a dose type of {self.dose_type}'
 
