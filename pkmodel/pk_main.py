b92966e48027525a21c866ba1b9c1f53aee1fb7e
dccotton <deborah.cotton@stx.ox.ac.uk>
2021-10-21 16:57:57 +0100

    commit_labelling

--- pkmodel/pk_main.py
+++ pkmodel/pk_main.py
@@ -1,10 +1,10 @@
 """Main code to run the whole model"""
 
 #import functions
-from create_model import *
+from create_model import * #  function to store the initial inputs into classes
 
 #import all the initial input values for the model
-from inputs import *
+from inputs import * # imports a dictionary of all the imputs for the models, and an integer for the number of models
 
 #put the model data into classes and create a solution class with a time array of the solution
 if number_of_models == 2:
