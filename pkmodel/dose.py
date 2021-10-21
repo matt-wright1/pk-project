c47fdd74a3f48e749e48f32b5bbce6f0b884a379
Matthew Wright <matthew.wright@env-res.ox.ac.uk>
2021-10-21 13:00:29 +0100

    #23 added dose testing ability, note slight error in dose file I want help with

--- pkmodel/dose.py
+++ pkmodel/dose.py
@@ -12,13 +12,23 @@ class Dose:
     def __init__(self, c_amount = 0, i_amount = 0, i_times = [0]):
         self.c_amount = self._is_data_valid(c_amount)
         self.i_amount = self._is_data_valid(i_amount)
-        self.i_times = self._is_data_valid(i_times)
+        if type(i_times) == 'list':
+            self.i_times = self._are_times_valid(i_times)
+        else:
+            self.i_times = [self._is_data_valid(i_times)]
 
     def _is_data_valid(self, value):
         if value < 0:
             raise ValueError("Entry cannot be negative")
         
         return value
+
+    def _are_times_valid(self, list):
+        for i in range(list):
+            if list[i] < 0:
+                raise ValueError("Entry cannot be negative")
+        
+        return list
     
     def __str__(self):
         return f"The current doseage is {self.c_amount} ng/hr and {self.i_amount} ng at times {self.i_times}"
