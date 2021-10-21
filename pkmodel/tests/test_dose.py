988d9037742fd63f0fc55d649fe29b8d10dbd5e6
Sol-sf <sol.quinn@live.co.uk>
2021-10-21 14:54:09 +0100

    #23 fixed errors in unittests

--- pkmodel/tests/test_dose.py
+++ pkmodel/tests/test_dose.py
@@ -6,19 +6,19 @@ class DoseTest(unittest.TestCase):
     """
     Tests the :class:`Dose` class.
     """
-    # def test_dose_defaults(self):
-    #     """
-    #     Tests dose class has correct default values of 0.
-    #     """
-    #     TestDose = Dose()
-    #     self.assertEqual([TestDose.c_amount, TestDose.i_amount, TestDose.i_times], [0, 0, [0]])
+    def test_dose_defaults(self):
+        """
+        Tests dose class has correct default values of 0.
+        """
+        TestDose = Dose()
+        self.assertEqual([TestDose.c_amount, TestDose.i_amount, TestDose.i_times], [0, 0, [0]])
 
-    # def test_dose_positives(self):
-    #     """
-    #     Tests dose class works with positive integers.
-    #     """
-    #     TestDose = Dose(7,10,5)
-    #     self.assertEqual([TestDose.c_amount, TestDose.i_amount, TestDose.i_times], [7, 10, [5]])
+    def test_dose_positives(self):
+        """
+        Tests dose class works with positive integers.
+        """
+        TestDose = Dose(7,10,5)
+        self.assertEqual([TestDose.c_amount, TestDose.i_amount, TestDose.i_times], [7, 10, [5]])
 
     def test_dose_negatives(self):
         """
@@ -34,13 +34,13 @@ class DoseTest(unittest.TestCase):
             TestDose = Dose(0, 0, [-5, 0 ,1])
 
 
-    # def test_dose_add_time(self):
-    #     """
-    #     Tests dose class correctly adds times to list.
-    #     """
-    #     TestDose = Dose(1, 1, 0)
-    #     TestDose.add_i_time(5)
-    #     self.assertEqual([TestDose.c_amount, TestDose.i_amount, TestDose.i_times], [1, 1, [0, 5]])
+    def test_dose_add_time(self):
+        """
+        Tests dose class correctly adds times to list.
+        """
+        TestDose = Dose(1, 1, 0)
+        TestDose.add_i_time(5)
+        self.assertEqual([TestDose.c_amount, TestDose.i_amount, TestDose.i_times], [1, 1, [0, 5]])
 
 if __name__ == '__main__':
     unittest.main()
