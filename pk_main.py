a0718bbed1026766c28102c4a95983146caa882d
Hannah Sanderson <92574098+Hannah-RS@users.noreply.github.com>
2021-10-22 16:35:49 +0100

    Fixed plotting file

--- pk_main.py
+++ pk_main.py
@@ -10,6 +10,7 @@ from pkmodel.inputs import *  # imports a dictionary of all the imputs for the m
 from pkmodel.createmodel import create_model
 from pkmodel.solver import solve
 from pkmodel.check_inputs import check_inputs
+from pkmodel.plot import double_plot, single_plot
 
 # put the model data into classes and create a solution class with a time array of the solution
 # check that the user inputs make physical sense
@@ -19,11 +20,12 @@ if number_of_models == 2:
     model1, model2, solution1, solution2 = create_model(2, model_1_inputs, model_2_inputs)
     sol1 = solve(model1, solution1)
     sol2 = solve(model2, solution2)
+    double_plot(sol1, sol2)
 else:
     check_inputs(1, model_1_inputs)
     model1, solution1 = create_model(1, model_1_inputs)
     sol = solve(model1, solution1)
+    single_plot(sol)
 
-
-if __name__ == '__main__':
-    pk_main()
+#if __name__ == '__main__':
+    #pk_main()
