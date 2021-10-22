1ff685af4b2b85f9f3e80d6bee45268e0f9b0cfd
Sol-sf <sol.quinn@live.co.uk>
2021-10-22 16:57:48 +0100

    #121 New tests for createmodel

--- pkmodel/tests/test_createmodel.py
+++ pkmodel/tests/test_createmodel.py
@@ -1,6 +1,5 @@
 import unittest
-import pkmodel
-from pkmodel import number_of_models, model_1_inputs, create_model
+from pkmodel import model, number_of_models, model_1_inputs, create_model, model_2_inputs
 
 
 class CreateModelTest(unittest.TestCase):
@@ -15,20 +14,21 @@ class CreateModelTest(unittest.TestCase):
     #"""Tests the right comparment information is submitted to the class"""
 
     # run the create model function
-    def build_model(self, number_of_models, model_1_inputs, model_2_inputs=None):
+    def test_createmodel(self, number_of_models = number_of_models, model_1_inputs = model_1_inputs, model_2_inputs = model_2_inputs):
         #put the model data into classes and create a solution class with a time array of the solution
         if number_of_models == 2:
             model1, model2, solution1, solution2 = create_model(2, model_1_inputs, model_2_inputs)      
         else:
             model1, solution1 = create_model(1, model_1_inputs)
 
-        self.assertEqual(model_1.c_amount, model_1_inputs["m1_continous_dose_amount"])
-        self.assertEqual(model_1.i_amount, model_1_inputs["m1_instantaneous_dose_amount"])
-        self.assertEqual(model_1.i_times, model_1_inputs["m1_dose_times"])
+        self.assertEqual(model1.time, model_1_inputs["m1_time"])
+        self.assertEqual(model1.timestep, model_1_inputs["m1_timestep"])
+        self.assertEqual(['central' in model1.compartments,'dose' in model1.compartments, 'peripheral_1' in model1.compartments, \
+            'peripheral_2' in model1.compartments], [True,True,True,True])
 
-        self.assertEqual(model_1.Central.v, model_1_inputs["m1_volume_c"])
-        self.assertEqual(model_1.Central.q, model_1_inputs["m1_q_c_initial"])
-        self.assertEqual(model_1.Central.CL, model_1_inputs["m1_CL"])
+        #self.assertEqual(model_1.Central.v, model_1_inputs["m1_volume_c"])
+        #self.assertEqual(model_1.Central.q, model_1_inputs["m1_q_c_initial"])
+        #self.assertEqual(model_1.Central.CL, model_1_inputs["m1_CL"])
 
 
 if __name__ == '__main__':
